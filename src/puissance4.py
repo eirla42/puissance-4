dfb94fbc4adee6b047694fd0bbfb6001c37a3e9d
eirla42 <math.poke@gmail.com>
2022-01-14 18:58:22 +0100

    Fix center window

--- src/puissance4.py
+++ src/puissance4.py
@@ -10,9 +10,9 @@ connect4App = Window(root)
 
 # Declaration of window (title, dimensions, etc..)
 root.title('Puissance 4')
+root.eval('tk::PlaceWindow . center')
 root.maxsize(params.NB_COLUMNS * params.CELL_WIDTH, (params.NB_LINES + 1) * params.CELL_WIDTH)
 root.minsize(params.NB_COLUMNS * params.CELL_WIDTH, (params.NB_LINES + 1) * params.CELL_WIDTH)
-root.eval('tk::PlaceWindow . center')
 
 # Show window
 root.mainloop()
