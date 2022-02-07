44042253102c9651d9ab4438d9ca627ea51cbf62
eirla42 <math.poke@gmail.com>
2022-02-07 08:23:06 +0100

    Set minimums to params

--- src/params/params.py
+++ src/params/params.py
@@ -1,3 +1,15 @@
-NB_LINES = 6
-NB_COLUMNS = 7
-CELL_WIDTH = 100
+NB_LINES = 6  # Minimum 4
+NB_COLUMNS = 7  # Minimum 4
+CELL_WIDTH = 100  # Reduce the width of a cell if the window don't fit with your screens
+
+# Set the minimum number of lines
+if NB_LINES < 4:
+    NB_LINES = 4
+
+# Set the minimum number of columns
+if NB_COLUMNS < 4:
+    NB_COLUMNS = 4
+
+# Set the minimum width
+if CELL_WIDTH < 50:
+    CELL_WIDTH = 50
